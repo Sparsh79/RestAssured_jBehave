620e85266c4a234d5d771dc75a02f76223301880
Sparsh <sparsh.bhardwaj@knoldus.com>
2022-01-20 19:39:18 +0530

    Added allure reports comptability

--- src/test/java/com/knoldus/Runner/DemoStory.java
+++ src/test/java/com/knoldus/Runner/DemoStory.java
@@ -1,6 +1,7 @@
 package com.knoldus.Runner;
 
 import com.knoldus.steps.DemoSteps;
+import io.tapack.allure.jbehave.AllureReporter;
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
 import org.jbehave.core.junit.JUnitStories;
@@ -9,17 +10,25 @@ import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 
+import java.io.Console;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.CONSOLE;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.HTML;
 
 public class DemoStory extends JUnitStories {
 
     @Override
     public Configuration configuration() {
+        if (super.hasConfiguration()) {
+            return super.configuration();
+        }
         return new MostUsefulConfiguration()
-                .useStoryReporterBuilder(
-                        new StoryReporterBuilder().withDefaultFormats()
-                                .withFormats(Format.HTML, Format.CONSOLE));
+                .useStoryReporterBuilder(new StoryReporterBuilder()
+                        .withDefaultFormats()
+                        .withFormats(CONSOLE, HTML)
+                        .withReporters(new AllureReporter()));
     }
 
     @Override
